7dcad4d9f1072006118d339d917a8a613ddc5fff
Podogo MacBook Air <podogomacbookair@MacBook-Air.local>
2020-06-21 22:43:45 +0200

    carousel first draft in use

--- src/components/languages.js
+++ src/components/languages.js
@@ -11,7 +11,7 @@ import reduxImg from "../images/redux.png"
 
 export default function Languages(){
 
-    let lpairs = [
+    const [lpairs, setLpairs] = useState([
         [reactImg, "React"], 
         [gatsbyImg, "Gastby"], 
         [pythonImg, "Python"], 
@@ -20,13 +20,32 @@ export default function Languages(){
         [reduxImg, "Redux"],
         [mongoImg, "Mongo DB"],
         [bootstrapImg, "Bootstrap"]
-    ];
+    ]);
+
+    useEffect(() => {
+        let timer = 3000;
+        let space = -95;
+        let marginal = space;
+        let newer = [...lpairs, ...lpairs];
+        const interval = setInterval(() => {
+            if (lpairs.length > 1 && window.innerWidth <= 680){//having this inside the interval ensures the funcitonality responds to screen width change
+                
+                setTimeout(() => {
+                    document.getElementById(0).style.marginLeft = (marginal + "%");
+                    marginal <= 95 ? marginal += 50 : marginal = space;
+                }, (timer/2));
+            } else {
+                document.getElementById(0).style.marginLeft = "0%";
+            }
+        }, timer)
+        return () => clearInterval(interval);
+    })
 
         return (
             <div className={styles.container}>
                 {lpairs.map((x, i) => {
                     return (
-                        <div key={i} className={styles.language}>
+                        <div id={i} key={i} className={styles.language}>
                             <div className={styles.imageContain}><span><img src={x[0]} alt="logo" /></span></div>
                             <div className={styles.title}>{x[1]}</div>
                         </div>
